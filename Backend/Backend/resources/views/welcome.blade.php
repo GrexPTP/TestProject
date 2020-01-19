fe5bd3a1746d70272f93feab3222fb5d056f7551
Phat Phan Tan <46423166+phatptce130319@users.noreply.github.com>
2020-01-19 13:58:58 +0700

    package file

--- Backend/Backend/resources/views/welcome.blade.php
+++ Backend/Backend/resources/views/welcome.blade.php
@@ -2,17 +2,28 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="theme-color" content="#000000" />
 
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
+        <link
+        href="//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css"
+        rel="stylesheet"
+      />
+      <link
+        href="//fonts.googleapis.com/css?family=Roboto:400,700,300"
+        rel="stylesheet"
+        type="text/css"
+      />
     </head>
     <body style="margin: 0">
-        <div id="content">  
+        <div id="root">  
         </div>
     </body>
     <script type="text/javascript" src="js/app.js"></script>
